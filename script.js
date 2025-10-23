ba020c6d9c9eef31852a283f74fee73d7a6858af
ferdicriuz <lderrickwealth@gmail.com>
2025-10-23 05:00:44 +0100

    added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// fetch("http://localhost:4000/order", {
-//   method: "POST",
-//   headers: { "Content-Type": "application/json" },
-//   body: JSON.stringify({ name, address, notes, cart }),
-// });
+fetch("http://localhost:4000/order", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ name, address, notes, cart }),
+});
